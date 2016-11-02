84632163ce513f191b6f0257c8c2e0cb2aeb85ea
Siraj <sirajravel@gmail.com>
2016-11-02 13:10:06 -0700

    Update RL.py

--- RL.py
+++ RL.py
@@ -1,20 +1,32 @@
 import tensorflow as tf
-import cv2
-import pong
-import numpy as np
-import random
-from collections import deque
+import cv2 #read in pixel data
+import pong #our class
+import numpy as np #math
+import random #random 
+from collections import deque #queue data structure. fast appends. and pops. replay memory
 
-ACTIONS = 3
+
+
+#hyper params
+ACTIONS = 3 #up,down, stay
+#define our learning rate
 GAMMA = 0.99
+#for updating our gradient or training over time
 INITIAL_EPSILON = 1.0
 FINAL_EPSILON = 0.05
-EXPLORE = 500000 # frames over which to anneal epsilon
+#how many frames to anneal epsilon
+EXPLORE = 500000 
 OBSERVE = 50000
+#store our experiences, the size of it
 REPLAY_MEMORY = 500000
+#batch size to train on
 BATCH = 100
 
+#create tensorflow graph
 def createGraph():
+
+    #first convolutional layer. bias vector
+    #creates an empty tensor with all elements set to zero with a shape
     W_conv1 = tf.Variable(tf.zeros([8, 8, 4, 32]))
     b_conv1 = tf.Variable(tf.zeros([32]))
 
@@ -30,9 +42,11 @@ def createGraph():
     W_fc5 = tf.Variable(tf.zeros([784, ACTIONS]))
     b_fc5 = tf.Variable(tf.zeros([ACTIONS]))
 
-    #input
+    #input for pixel data
     s = tf.placeholder("float", [None, 84, 84, 4])
 
+
+    #Computes rectified linear unit activation fucntion on  a 2-D convolution given 4-D input and filter tensors. and 
     conv1 = tf.nn.relu(tf.nn.conv2d(s, W_conv1, strides = [1, 4, 4, 1], padding = "VALID") + b_conv1)
 
     conv2 = tf.nn.relu(tf.nn.conv2d(conv1, W_conv2, strides = [1, 2, 2, 1], padding = "VALID") + b_conv2)
@@ -47,22 +61,34 @@ def createGraph():
 
     return s, fc5
 
+
+#deep q network. feed in pixel data to graph session 
 def trainGraph(inp, out, sess):
-    argmax = tf.placeholder("float", [None, ACTIONS]) #to calculate the argmax, we multiply the predicted output with a vector with one value 1 and rest as 0
+
+    #to calculate the argmax, we multiply the predicted output with a vector with one value 1 and rest as 0
+    argmax = tf.placeholder("float", [None, ACTIONS]) 
     gt = tf.placeholder("float", [None]) #ground truth
 
+    #action
     action = tf.reduce_sum(tf.mul(out, argmax), reduction_indices = 1)
+    #cost function we will reduce through backpropagation
     cost = tf.reduce_mean(tf.square(action - gt))
+    #optimization fucntion to reduce our minimize our cost function 
     train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
+    #initialize our game
     game = pong.PongGame()
     
+    #create a queue for experience replay to store policies
     D = deque()
 
     #intial frame
     frame = game.getPresentFrame()
+    #convert rgb to gray scale for processing
     frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
+    #binary colors, black or white
     ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
+    #stack frames, that is our input tensor
     inp_t = np.stack((frame, frame, frame, frame), axis = 2)
 
     #saver
@@ -73,9 +99,14 @@ def trainGraph(inp, out, sess):
     t = 0
     epsilon = INITIAL_EPSILON
     
+    #training time
     while(1):
+        #output tensor
         out_t = out.eval(feed_dict = {inp : [inp_t]})[0]
+        #argmax function
         argmax_t = np.zeros([ACTIONS])
+
+        #
         if(random.random() <= epsilon):
             maxIndex = random.randrange(ACTIONS)
         else:
@@ -85,17 +116,26 @@ def trainGraph(inp, out, sess):
         if epsilon > FINAL_EPSILON:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
+        #reward tensor if score is positive
         reward_t, frame = game.getNextFrame(argmax_t)
+        #get frame pixel data
         frame = cv2.cvtColor(cv2.resize(frame, (84, 84)), cv2.COLOR_BGR2GRAY)
         ret, frame = cv2.threshold(frame, 1, 255, cv2.THRESH_BINARY)
         frame = np.reshape(frame, (84, 84, 1))
+        #new input tensor
         inp_t1 = np.append(frame, inp_t[:, :, 0:3], axis = 2)
         
+        #add our input tensor, argmax tensor, reward and updated input tensor tos tack of experiences
         D.append((inp_t, argmax_t, reward_t, inp_t1))
+
+        #if we run out of replay memory, make room
         if len(D) > REPLAY_MEMORY:
             D.popleft()
         
+        #training iteration
         if t > OBSERVE:
+
+            #get values from our replay memory
             minibatch = random.sample(D, BATCH)
         
             inp_batch = [d[0] for d in minibatch]
@@ -106,29 +146,36 @@ def trainGraph(inp, out, sess):
             gt_batch = []
             out_batch = out.eval(feed_dict = {inp : inp_t1_batch})
             
+            #add values to our batch
             for i in range(0, len(minibatch)):
                 gt_batch.append(reward_batch[i] + GAMMA * np.max(out_batch[i]))
 
+
+
+            #train on that 
             train_step.run(feed_dict = {
                            gt : gt_batch,
                            argmax : argmax_batch,
                            inp : inp_batch
                            })
         
+        #update our input tensor the the next frame
         inp_t = inp_t1
         t = t+1
 
+        #print our where wer are after saving where we are
         if t % 10000 == 0:
             saver.save(sess, './' + 'pong' + '-dqn', global_step = t)
 
-        print "TIMESTEP", t, "/ EPSILON", epsilon, "/ ACTION", maxIndex, "/ REWARD", reward_t, "/ Q_MAX %e" % np.max(out_t)
+        print("TIMESTEP", t, "/ EPSILON", epsilon, "/ ACTION", maxIndex, "/ REWARD", reward_t, "/ Q_MAX %e" % np.max(out_t))
 
 
 def main():
-    print "Start"
+    #create session
     sess = tf.InteractiveSession()
+    #input layer and output layer by creating graph
     inp, out = createGraph()
-    print "Created Graph"
+    #train our graph on input and output with session variables
     trainGraph(inp, out, sess)
 
 if __name__ == "__main__":
